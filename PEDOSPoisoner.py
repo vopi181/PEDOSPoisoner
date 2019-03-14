235a60ed79ace2d464fac0c1d42783c6f6924db9
Dominic Pace <dompace181@gmail.com>
2019-03-14 14:26:58 -0500

    implementing change in stub size

--- PEDOSPoisoner.py
+++ PEDOSPoisoner.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import argparse
 import pefile
 
@@ -13,14 +12,18 @@ def inject_stub(pe_file, stub_file):
     pe_data = pefile.PE(pe_file)
     dos_header_dict = pe_data.DOS_HEADER.dump_dict()
     pe_offset = dos_header_dict.get("e_lfanew")['Value']
+    old_dos_stub_size = pe_offset - 64
     old_pe_dos_stub = pe_file_contents[64:pe_offset]
     
     print("[*] Reading DOS stub file...")
     with open(stub_file, 'rb') as f:
         dos_stub_contents = f.read()
     print("[*] Replacing and writing stub...")
-  
+
     with open(pe_file, 'r+b') as f:
+        if(len(dos_stub_contents) > old_dos_stub_size):
+            f.seek(0x3d)
+            f.write((len(dos_stub_contents) + ((len(dos_stub_contents) - old_dos_stub_size))))
         f.seek(64)
         f.write(dos_stub_contents)
         
